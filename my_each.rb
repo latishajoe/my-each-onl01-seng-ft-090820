0096b2d174cfb12cd7ce46e71865dc3d6a4d0486
Latisha Joe <latisha_joe@yahoo.com>
2020-09-16 12:34:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,17 @@
 def my_each(array)
   i = 0
-  while i < array.length 
-    yield(array[i]) 
-    i = i + 1 
-  end 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
 
 def my_each(array)
-  i = 0 
+  i = 0
   collection = []
   while i < array.length
    collection << yield(array[i])
    i += 1
- end 
- collection 
+ end
+ collection
 end 
